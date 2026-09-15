5c1289bbc5447d0bc828fb9eb7bbdb74e7783673
Joost van Schie <8194979+joost-van-schie@users.noreply.github.com>
2026-09-15 16:28:28 +0000

    fix: beheerpaneel toegankelijk buiten local (FilamentUser::canAccessPanel)

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -12,10 +14,21 @@ use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    /**
+     * Wie mag in het beheerpaneel (/admin)? Elke gebruiker met een account.
+     * Buiten je eigen computer laat Filament anders niemand binnen. Wil je
+     * alleen beheerders toelaten, voeg dan een kolom `is_beheerder` toe en
+     * controleer die hier.
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 
     /**
      * Get the attributes that should be cast.
